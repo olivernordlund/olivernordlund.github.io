c064bd6ebb05108471a50a477cb76629a2c5e90a
Oliver Nordlund <oliver.nordlund@gmail.com>
2026-02-20 19:04:05 +0100

    abc

--- script.js
+++ script.js
@@ -46,14 +46,18 @@ const rotations = [
     "wg-rotate-none"
 ];
 
-function shuffle(array) {
-    return array.sort(() => Math.random() - 0.5);
-}
-
 async function loadImages() {
     const response = await fetch("images.json", { cache: "no-store" });
     if (!response.ok) throw new Error("Could not load images.json");
     return response.json();
+}
+
+function shuffleCopy(arr) {
+    return [...arr].sort(() => Math.random() - 0.5);
+}
+
+function inRange(n, min, max) {
+    return n >= min && n <= max;
 }
 
 async function buildGallery() {
@@ -63,9 +67,19 @@ async function buildGallery() {
 
     const images = await loadImages();
 
-   images
-  .sort((a, b) => (a.order ?? 999) - (b.order ?? 999))
-  .forEach(item => {
+    // Kapitlen (baserat på order)
+    const vigsel = images.filter(i => inRange(i.order ?? 999, 1, 24));
+    const resa = images.filter(i => inRange(i.order ?? 999, 25, 49));
+    const on = images.filter(i => inRange(i.order ?? 999, 50, 74));
+
+    // Random inom varje kapitel, men kapitlen i fast ordning
+    const finalImages = [
+        ...shuffleCopy(vigsel),
+        ...shuffleCopy(resa),
+        ...shuffleCopy(on)
+    ];
+
+    finalImages.forEach(item => {
         const div = document.createElement("div");
         div.className = `wg-item ${rotations[Math.floor(Math.random() * rotations.length)]}`;
 
